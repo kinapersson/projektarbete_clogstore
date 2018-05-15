f6c1c53dfc3ba8389992284b0c5a926e91dfa6e6
kinapersson <persson.kina@gmail.com>
2018-05-15 21:44:42 +0200

    added remove order function

--- admin_controllers/admin_manageOrders_controller.php
+++ admin_controllers/admin_manageOrders_controller.php
@@ -1,6 +1,16 @@
 <?php
+//All orders will be shown here (overview)
 
-
+function removeOrder(){
+    global $dbh;
+    $id = $_POST['removeID'];
+    $sql = "DELETE FROM orders WHERE OID='$id'";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+}
+if(isset($_POST['removeID'])){
+    removeOrder();
+}
 
 $sql = "SELECT * FROM orders";
 
@@ -11,4 +21,11 @@ $viewOrders = $stmt->fetchAll(PDO::FETCH_OBJ);
 
 
 loadTemplate("manageOrders", $viewOrders);
+
+
+
+
+
+
+
 ?>
