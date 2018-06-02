951eff7096371644985d7ea417f0188d07218454
Marie Sahlén <mariesahlen1@gmail.com>
2018-06-02 05:01:12 +0200

    Uppdaterat checkout form

--- templates/checkout.tpl.php
+++ templates/checkout.tpl.php
@@ -6,88 +6,49 @@ require 'templates/cart.tpl.php';
 
 ?>
 
-
-
-<section class="container fluid" id="cart_form" style="padding-top:25px">
-	<div class="col-lg-12 col-md-12 col-sm-12 col-sx-12">
-
-
-
-<form method="post" action="?controller=place_order">
-    <h3 class="panel-title">Contact information</h3>
-    <div class="panel-body">
-    <div class="form-row">
-      <div class="form-group col-md-6">
-        <label for="inputName">Name</label>
-        <input type="name" class="form-control" id="inputName" placeholder="Name">
-      </div>
-      <div class="form-group col-md-6">
-        <label for="inputSurname">Surname</label>
-        <input type="surname" class="form-control" id="inputPassword4" placeholder="Surname">
-      </div>
-    </div>
-    <div class="form-row">
-      <div class="form-group col-md-6">
-        <label for="inputEmail4">Email</label>
-        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-      </div>
-      <div class="form-group col-md-6">
-        <label for="inputPhone4">Phone</label>
-        <input type="phone" class="form-control" id="inputPhone" placeholder="Phone">
-      </div>
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputAddress">Address</label>
-      <input type="text" class="form-control" id="inputAddress" placeholder="Street name and number">
-    </div>
-    <div class="form-row">
-      <div class="form-group col-md-6">
-        <label for="inputCity">City</label>
-        <input type="text" class="form-control" id="inputCity">
-      </div>
-      <div class="form-group col-md-6">
-      <label for="inputAddress">Zip</label>
-      <input type="text" class="form-control" id="inputAddress" placeholder="Street name and number">
-    </div>
-    <div class="form-row">
-      <div class="form-group col-md-6">
-        <label for="inputCity">Country</label>
-        <input type="text" class="form-control" id="inputCity">
-      </div>
-      
-    </div>
-  
-	  <h3 class="panel-title">Delivery Address</h3>
-
-<div class="form-group col-md-6">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="Street name and number">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-6">
-    <label for="inputAddress">Zip</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="Street name and number">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">Country</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    
-  </div>
-
-  
-  <button type="submit" class="btn btn-primary">Sign in</button>
-   <button type="submit" class="btn btn-primary">Place Order</button>
-   <div>
-
-</div>
+<div class="container" style="padding-top:25px">
+  <div class="wrapper">
+    <div class="col-lg-9 col-md-12 col-sm-12 col-sx-12">
  
-</form>
+        <form method="post" action="?controller=checkout">
+          <div class="form-group">
+            <label name="OrderCreateName">User Name</label>
+            <input name="userName" type="text" class="form-control" placeholder="Write your user name" value=""/>
+            <label name="OrderCreateEmail">E-mail</label>
+            <input name="userEmail" type="text" class="form-control" placeholder="Write your e-mail" value=""/>
+            <label name="OrderCreatePhoneNumber">Phone Number</label>
+            <input name="userMobileNumber" type="text" class="form-control" placeholder="Write your phone number" value=""/>
+            <label name="OrderCreateWorkNumber">Contact Number Work</label>
+            <input name="WorkNumber" type="text" class="form-control" placeholder="Write your contact number work" value=""/>
+
+            <label name="OrderCreateAddressDelivery">Delivery Address</label>
+            <input name="userAddressDelivery" type="text" class="form-control" placeholder="Write your delivery address" value=""/>
+            <label name="OrderCreateZipcodeDelivery">Delivery Zip Code</label>
+            <input name="userZipcodeDelivery" type="text" class="form-control" placeholder="Write your deilvery zip code" value=""/>
+            <label name="OrderCreateCityDelivery">Delivery City</label>
+            <input name="userCityDelivery" type="text" class="form-control" placeholder="Write your delivery city" value=""/>
+
+            <label name="OrderCreateAddressInvoice">Billing Address</label>
+            <input name="userAddressInvoice" type="text" class="form-control" placeholder="Write your billing address" value=""/>
+            <label name="OrderCreateZipcodeInvoice">Billing Address Zip Code</label>
+            <input name="userZipcodeInvoice" type="text" class="form-control" placeholder="Write your billing address zip code" value=""/>
+            <label name="OrderCreateCityInvoice">Billing Address City</label>
+            <input name="userCityInvoice" type="text" class="form-control" placeholder="Write your billing Address City" value=""/>
+            
+            <label name="OrderCreatePassword">Create a Password</label>
+            <input name="userPassword" type="text" class="form-control" placeholder="Create a password" value=""/>
+            
+            <label name="OrderCreatePassword">Payment Method </label>
+            <label class="radio-inline">
+            <input type="radio" name="optradio">Credit Card</label>
+            <label class="radio-inline">
+            <input type="radio" name="optradio">Bank Transfer</label>
+          </div>
+          <button type="submit" class="btn btn-primary">Place Order</button>
+        </form>
+      </div>
+    </div>
+  </div>
 
 
 		
